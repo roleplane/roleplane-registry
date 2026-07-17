1d334c48ef08293dfb2ead4956a0e9624ef0f2cc
piwero <piwero@gmail.com>
2026-07-17 06:00:00 +0200

    chore: mise task pushing Pages OAuth secrets from 1Password; ignore .wrangler build dir

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["node_modules", "dist"] },
+  { ignores: ["node_modules", "dist", ".wrangler"] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
 );
